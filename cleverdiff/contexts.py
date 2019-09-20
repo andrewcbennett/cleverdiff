263833c779fd0fa8286f631e0159d70d1556d627
Andrew <andrewcbennett@users.noreply.github.com>
2019-09-20 12:10:05 +0100

    Improves documentation in contexts module

--- cleverdiff/contexts.py
+++ cleverdiff/contexts.py
@@ -1,18 +1,34 @@
+"""
+Defines classes for providing the context of different file types.
+See DefaultContext for the interface.
+
+The types in this module should be passed into the `difflist.DiffList` constructor
+to specify how to translate line numbers into human-readable strings.
+"""
+
+
 class DefaultContext:
+    """A generic context which just returns line numbers."""
     DESCRIPTION = "unformatted text file"
 
     def __init__(self, filename):
-        self.label = filename
+        self.filename = filename
 
-    def __getitem__(self, item):
-        return f"{self.label}:{item}"
+    def __getitem__(self, line):
+        """Returns the filename and given line number as a string."""
+        return f"{self.filename}:{line}"
 
 
 class EcflowContext:
+    """
+    A context class that understands ecFlow suite definitions. These are
+    documented at
+    https://confluence.ecmwf.int/display/ECFLOW/Definition+file+Grammar.
+    """
     DESCRIPTION = "ecFlow suite definition"
 
     def __init__(self, filename):
-        self.label = filename
+        self.filename = filename
         with open(filename, "rt") as f:
             self._context = self._create_context(f.read().splitlines())
 
@@ -20,7 +36,7 @@ class EcflowContext:
     def _create_context(cls, content):
         """
         Given string content, establish a human-readable context for each
-        line.
+        line. This will be the path of the current node at each line.
 
         Arguments
         ---------
@@ -31,7 +47,9 @@ class EcflowContext:
         -------
         list
             A list of strings describing each line of the content.
+
         """
+
         result = []
         stack = [""]
         last_kw = None
@@ -55,13 +73,31 @@ class EcflowContext:
         return result
 
     def __getitem__(self, item):
-        return f"{self.label}:{self._context[item]}"
+        return f"{self.filename}:{self._context[item]}"
 
 
+# A dict mapping filename extensions to context classes
 EXTENSION_MAPPING = {
     ".def": EcflowContext,
 }
 
 
 def lookup_extension(extension):
+    """
+    Returns a class type for a given filename extension. If the
+    requested extension does not have an associated class type,
+    returns `DefaultContext`.
+
+    Arguments
+    ---------
+
+    extension : str
+        The extension of the filename, including the leading dot.
+
+    Returns
+    -------
+    type
+        A class name representing the context.
+
+    """
     return EXTENSION_MAPPING.get(extension, DefaultContext)
