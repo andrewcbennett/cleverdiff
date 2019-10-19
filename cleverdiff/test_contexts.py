1e4f13efc450e3db8c28b031556a77d45c89ace8
Andrew <andrewcbennett@users.noreply.github.com>
2019-10-19 14:55:54 +0100

    black reformat

--- cleverdiff/test_contexts.py
+++ cleverdiff/test_contexts.py
@@ -16,7 +16,7 @@ class Test_DefaultContext:
 
     def test_getindex(self):
         """Ensure that dict notation returns a sensible string."""
-        filename = '/path/to/foobar'
+        filename = "/path/to/foobar"
         instance = DefaultContext(filename)
 
         actual = instance[3]
@@ -39,10 +39,12 @@ class Test_EcflowContext:
 
     def test_getindex(self):
         """Ensure that dict notation returns a sensible string."""
-        filename = '/path/to/foobar'
+        filename = "/path/to/foobar"
 
-        with patch("cleverdiff.contexts.EcflowContext._create_context",
-                   return_value=["/first", "/second", "/third"]):
+        with patch(
+            "cleverdiff.contexts.EcflowContext._create_context",
+            return_value=["/first", "/second", "/third"],
+        ):
             with tempfile.NamedTemporaryFile(mode="wt", delete=False) as f:
                 filename = f.name
                 instance = EcflowContext(filename)
@@ -57,7 +59,8 @@ class Test_EcflowContext:
 
     def test_create_context(self):
         """Ensure that _create_context classmethod behaves as expected."""
-        content = textwrap.dedent("""
+        content = textwrap.dedent(
+            """
             suite foo
               limit BAZLIM 10
               family bar
@@ -73,7 +76,8 @@ class Test_EcflowContext:
               task end
                 trigger bar == complete
             endsuite
-        """).splitlines(True)
+        """
+        ).splitlines(True)
         expected = [
             "",
             "/foo",
